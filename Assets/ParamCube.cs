f867c214436f3111c48538c3289b6ea8129dafd3
koedijk <17798@ma-web.nl>
2018-04-18 09:22:40 +0200

    Begin Rhythm Detection

---
+++ Assets/ParamCube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    public bool _useBuffer;
+    public bool _testBeat;
+    public float _beatsensivity;
+    private float _old = 0, _new;
+    Material _material;
+	// Use this for initialization
+	void Start () {
+        _material = GetComponent<MeshRenderer>().materials[0];
+	}
+
+    void TestBeat()
+    {
+        if (_testBeat)
+        {
+            _new = AudioPeer._bandBuffer[_band];
+            if (_new - _old > _beatsensivity)
+            {
+                Debug.Log("Beat");
+            }
+            _old = _new;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+        if (_useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._bandBuffer[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+            Color _color = new Color(AudioPeer._audioBandBuffer[_band], AudioPeer._audioBandBuffer[_band], AudioPeer._audioBandBuffer[_band]);
+            _material.SetColor("EmissionColor", _color);
+        }
+        else if (!_useBuffer) {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+            Color _color = new Color(AudioPeer._audioBandBuffer[_band], AudioPeer._audioBandBuffer[_band], AudioPeer._audioBandBuffer[_band]);
+            _material.SetColor("EmissionColor", _color);
+        }
+        TestBeat();
+	}
+}
---
+++ Music Test/Assets/InstatiateCubes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateCubes : MonoBehaviour {
+    public GameObject _samplePrefab;
+    GameObject[] _sampleCube = new GameObject[512];
+    public float _maxScale;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _instaceSampleCube = (GameObject)Instantiate(_samplePrefab);
+            _instaceSampleCube.transform.position = this.transform.position;
+            _instaceSampleCube.transform.parent = this.transform;
+            _instaceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instaceSampleCube.transform.position = Vector3.forward * 100;
+            _sampleCube[i] = _instaceSampleCube;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 512; i++)
+        {
+            if (_sampleCube != null) {
+                _sampleCube[i].transform.localScale = new Vector3(1, (AudioPeer._samples[i] * _maxScale) + 2, 1);
+            }
+        }
+	}
+}
