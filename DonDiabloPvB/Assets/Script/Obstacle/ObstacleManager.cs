4671dd9d8bc850efcaf3a53c480bf3c34317309e
Sebastiaan Kappert <sebaskappert@gmail.com>
2018-05-14 16:22:32 +0200

    started work on obstacles, finished first version of distance calculations.

---
+++ DonDiabloPvB/Assets/Script/Obstacle/ObstacleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+    private int disToPlayer;
+    private bool IsDestroyed;
+    private Vector3 playerPos;
+
+    private void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag(Tags.PlayerTag).transform.position;
+
+    }
+   
+     private int GetDistanceToPlayer()
+    {
+        float dis = transform.position.z - playerPos.z;
+        return 0;
+         
+    }
+
+}
--- DonDiabloPvB/Assets/Script/Tags.cs
+++ DonDiabloPvB/Assets/Script/Tags.cs
@@ -3,5 +3,6 @@
 public class Tags{
 
     public static string SpawnerTag = "TrackSpawner";
-
+    public static string PlayerTag = "Player"
+;
 }
