7b0d2bd8d7b4ae7b3a1e0f93b618d69c8f3bc0b1
Jochem <joaghim.beltman@gmail.com>
2018-05-14 14:37:37 +0200

    git fixes & development on tracks

--- DonDiabloPvB/Assets/Script/LevelTrack/Examples/PathPlacer.cs
+++ DonDiabloPvB/Assets/Script/LevelTrack/Examples/PathPlacer.cs
@@ -7,15 +7,24 @@ public class PathPlacer : MonoBehaviour {
 
     public float spacing = 1f;
     public float resolution = 1;
-    //public GameObject road;
+    public GameObject playerTrackPoint;
 
     private void Start(){
         Vector2[] points = FindObjectOfType<PathCreator>().path.CalculateEvenSpacePoints(spacing, resolution);
+        /*
         foreach (Vector2 p in points){
-            GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            //var g = (GameObject)Instantiate(road);
+            //GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            var g = (GameObject)Instantiate(playerTrackPoint);
             g.transform.position = p;
-            g.transform.localScale = Vector3.one * spacing * 20f;
+            g.transform.localScale = Vector3.one * spacing * 0.5f;
+        }
+        */
+
+        for (int i = 0; i < points.Length; i++){
+            var g = (GameObject)Instantiate(playerTrackPoint, transform);
+            g.transform.position = points[i];
+            g.transform.localScale = Vector3.one * spacing * 0.5f;
+            g.GetComponent<PlayerTrackingPoint>().PointIndex = i;
         }
 
         /*
@@ -26,7 +35,7 @@ public class PathPlacer : MonoBehaviour {
                 g.transform.localScale = Vector3.one * spacing * 20f;
                 g.transform.LookAt(points[0]);
             }else{
-                g.transform.position = points[i];
+                g.transform.position = points[i+1];
                 g.transform.localScale = Vector3.one * spacing * 20f;
                 g.transform.LookAt(points[i+1]);
             }
