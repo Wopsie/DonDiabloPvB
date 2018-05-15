14f3e2d561b1fda72d5a8ea282523a22cf749682
Niels <Niels97vet@hotmail.com>
2018-05-15 10:43:16 +0200

    Pulled from development branch and added to the shader branch

---
+++ DonDiabloPvB/Assets/Script/LevelTrack/Splines/PathCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SplineEditor { 
+
+    public class PathCreator : MonoBehaviour {
+
+        [HideInInspector]
+	    public Path path;
+
+        public void CreatePath(){
+            path = new Path(transform.position);
+        }
+    }
+}
