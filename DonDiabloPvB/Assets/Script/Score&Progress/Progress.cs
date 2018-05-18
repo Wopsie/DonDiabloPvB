7004a3359148ab21d464c8a2d0576b8245876dcc
koedijk <17798@ma-web.nl>
2018-05-18 11:17:01 +0200

    Commit

---
+++ DonDiabloPvB/Assets/Script/Score&Progress/Progress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progress : MonoBehaviour {
+    private Score scoreScript;
+	// Use this for initialization
+	void Awake () {
+        scoreScript = GameObject.Find("Score").GetComponent<Score>();
+        UpdateProgress("1",scoreScript.highScore);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateProgress(string name,int score)
+    {
+        PlayerPrefs.SetInt(name, score);
+        Debug.Log(PlayerPrefs.HasKey("0"));
+    }
+}
---
+++ DonDiabloPvB/Assets/Script/Score&Progress/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    private string songName;
+    public int highScore;
+    private int amount = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetSong(string name = "0")
+    {
+        songName = name;
+    }
+
+    void AddScore()
+    {
+        highScore = highScore + amount;
+    }
+}
