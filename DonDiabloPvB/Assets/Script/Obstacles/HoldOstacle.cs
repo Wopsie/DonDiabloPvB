1706fb040e848d3951dc2ae2bd8f3c4a0ae26fff
Jochem <joaghim.beltman@gmail.com>
2018-05-25 14:07:04 +0200

    Additions to obstacle test scene & Hold obstacle

---
+++ DonDiabloPvB/Assets/Script/Obstacles/HoldOstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldOstacle : Obstacle {
+
+    [SerializeField]
+    private ObstacleType type;
+    [SerializeField]
+    private new ShieldState reqShieldState = ShieldState.HoldShield;
+
+    private void Awake(){
+        SetInduvidualData();
+    }
+
+    private void Update(){
+        base.CheckPlayerDistances();
+    }
+
+    protected override void OnPlayerCollision(){
+        if(helper.player.currShieldState == reqShieldState){
+            //success
+            helper.AddScore(scoreToAward);
+            Debug.Log("<color=green>VERRY G00D</color>");
+        }else{
+            helper.player.Reset();
+            Debug.Log("<color=red>SUKKEL</color>");
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll){
+        if (waypointPositionIndex == 0)
+            base.SnapToClosestTrackPoint(coll);
+
+        if (coll.gameObject.tag == Tags.PlayerTag)
+            OnPlayerCollision();
+    }
+}
