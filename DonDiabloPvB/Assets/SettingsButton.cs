a197c338784aca8a9f0ffcc837d50bb0b7a31599
koedijk <17798@ma-web.nl>
2018-05-29 11:55:16 +0200

    Added Art Pauze screen

---
+++ DonDiabloPvB/Assets/SettingsButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour {
+
+    private GameObject settings;
+
+    private void OnMouseDown()
+    {
+        Time.timeScale = 0;
+        //settings.SetActive(true);
+    }
+}
