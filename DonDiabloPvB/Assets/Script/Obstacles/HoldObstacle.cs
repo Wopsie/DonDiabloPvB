3132e6e924e8cb65746f53db9fe6cc2536592526
Jochem <joaghim.beltman@gmail.com>
2018-06-05 13:54:12 +0200

    Cleaned up obstacle scripts, added particle effects to obstacles & added other particles to be implemented.

--- DonDiabloPvB/Assets/Script/Obstacles/HoldObstacle.cs
+++ DonDiabloPvB/Assets/Script/Obstacles/HoldObstacle.cs
@@ -16,17 +16,6 @@ public class HoldObstacle : Obstacle {
         base.CheckPlayerDistances();
     }
 
-    protected override void OnPlayerCollision(){
-        if (helper.player.currShieldState == reqShieldState){
-            //success
-            helper.AddScore(scoreToAward);
-            Debug.Log("<color=green>VERRY G00D</color>");
-        }else{
-            helper.player.Reset();
-            Debug.Log("<color=red>SUKKEL</color>");
-        }
-    }
-
     private void OnTriggerEnter(Collider coll){
         if (waypointPositionIndex == 0)
             base.SnapToClosestTrackPoint(coll);
