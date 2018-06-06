d0105f2e641d9feb8d98fd6cbbdd65aed96805ab
Niels <Niels97vet@hotmail.com>
2018-06-06 14:33:19 +0200

    Graybox scene fixed, added ui. Menu Final!!!

--- DonDiabloPvB/Assets/Script/UI/StartButton.cs
+++ DonDiabloPvB/Assets/Script/UI/StartButton.cs
@@ -21,19 +21,17 @@ public class StartButton : MonoBehaviour
     }
     #endregion
 
-    [SerializeField] private List<Sprite> _levelSelectScreens = new List<Sprite>();
     [SerializeField] private GameObject _menuUI;
     [SerializeField] private Text levelNumberText;
 
     private AudioSource _audioSource;
     private LevelManager loader;
     private int _level;
-    private Image _imageRenderer;
 
     public void ButtonSelect()
     {
-        string levelName = "Level" + _level.ToString();
-        loader.PlaceLevel(levelName);
+        //string levelName = "Level" + _level.ToString();
+        //loader.PlaceLevel(levelName);
         Debug.Log(_level);
         _audioSource.Play();
         ShaderController.Instance.TriggerEffect(1);
@@ -43,14 +41,6 @@ public class StartButton : MonoBehaviour
     public void PassLevelNumber(int levelNumber)
     {
         _level = levelNumber;
-
-        if (_level >= 0){
-            Debug.Log("Passed valid number");
-            levelNumberText.text = levelNumber.ToString();
-        }else{
-            Debug.Log("Passed invalid number");
-            levelNumberText.text = "No level selected";
-        }
     }
 
     public void ToChangeLevel()
@@ -61,12 +51,22 @@ public class StartButton : MonoBehaviour
     private void Awake()
     {
         _audioSource = Camera.main.GetComponent<AudioSource>();
-        _imageRenderer = this.gameObject.GetComponent<Image>();
         loader = FindObjectOfType<LevelManager>();
     }
 
     private void ChangeLevelScreen()
     {
-        _imageRenderer.sprite = _levelSelectScreens[_level];
+        PassLevelNumber(_level);
+
+        if (_level >= 0)
+        {
+            Debug.Log("Passed valid number");
+            levelNumberText.text = _level.ToString();
+        }
+        else
+        {
+            Debug.Log("Passed invalid number");
+            levelNumberText.text = "No level selected";
+        }
     }
 }
